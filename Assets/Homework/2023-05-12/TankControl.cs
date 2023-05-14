52f1d1d34f33748a2e67a858c2b37dde3f481afe
YongJoon_Kim <be2163@korea.ac.kr>
2023-05-14 21:54:09 +0900

    feat. HW complete

--- Assets/Homework/2023-05-12/TankControl.cs
+++ Assets/Homework/2023-05-12/TankControl.cs
@@ -1,46 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TankControl : MonoBehaviour
 {
-    Rigidbody rb;
-    public float moveForce;
-    public float jumpForce;
-    public float rotateForce;
-    public float h;
-    public float v;
+    public float moveForce = 5.0f;
+    public float jumpForce = 1.5f;
+    public float rotateForce = 90.0f;
+    private Vector3 moveDir;
 
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-
-        moveForce = 1.5f;
-        jumpForce = 1.5f;
-        rotateForce = 1.5f;
-    }
-    public void FixedUpdate()
+    private void Update()
     {
         Move();
-        Jump();
+        Rotate();
+        //Rotation();
     }
     public void Move()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-
-        Vector3 dir = new Vector3(h, 0, v);
-
-        transform.Translate(dir * moveForce * Time.deltaTime);
-        transform.Rotate(Vector3.up, Time.deltaTime, Space.Self);
+        //transform.position += moveForce * moveDir * Time.deltaTime;
+        transform.Translate(moveDir * moveForce * Time.deltaTime, Space.Self);
+    }
+    public void Rotate()
+    {
+        //transform.LookAt(moveDir);
+        transform.Rotate(Vector3.up * moveDir.x * rotateForce * Time.deltaTime, Space.Self);
+    }
+    //public void LookAt()
+    //{
+    //    transform.LookAt(new Vector3(0, 0, 0));
+    //}
+    private void OnMove(InputValue value)
+    {
+        moveDir.x = value.Get<Vector2>().x;
+        moveDir.y = 0.0f;
+        moveDir.z = value.Get<Vector2>().y;
+    }
+    public void Rotation()
+    {
+        transform.rotation = Quaternion.identity;
+        transform.rotation = Quaternion.Euler(moveDir.x, 0, moveDir.z);
+        //transform.rotation.ToEulerAngles();
+    }
+    private void OnJump(InputValue value)
+    {
+        Jump();
     }
     public void Jump()
     {
-        if (Input.GetButton("Jump"))
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+        transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
+        
     }
 
 
